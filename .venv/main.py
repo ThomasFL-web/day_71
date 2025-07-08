68f41d9d669e1f1bdde9379fa63d780d4b5ef919
Thomas Flerko <thomas.flerko@yandex.com>
2025-07-08 18:32:44 +0200

    Use env variables and create Procfile.

--- .venv/main.py
+++ .venv/main.py
@@ -31,6 +31,9 @@ This will install the packages from the requirements.txt for this project.
 
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
@@ -58,7 +61,10 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{os.path.join(app.root_path, 'instance', 'posts.db')}"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+
+
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
@@ -301,4 +307,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=False, port=5001)
